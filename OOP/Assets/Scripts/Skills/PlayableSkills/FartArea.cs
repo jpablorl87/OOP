9593adcd8cf3cab47d66e67e0bb9293db3c5c551
Alejandro Berrio Gutierrez <codeberrio@gmail.com>
2025-05-13 06:11:11 -0500

    FartArea finished

--- OOP/Assets/Scripts/Skills/PlayableSkills/FartArea.cs
+++ OOP/Assets/Scripts/Skills/PlayableSkills/FartArea.cs
@@ -1,26 +1,53 @@
 using UnityEngine;
+
 [CreateAssetMenu(fileName = "New Fart", menuName = "Skills/FartArea")]
 public class FartArea : Skill
 {
-    public GameObject fartZone;
-    public float fartDuration;
-    public float fartDamage;
+    [Header("Settings")]
+    public GameObject fartZone;           
+    public float fartDuration = 2f;       
+    public float fartRadius   = 3f;       
+    public LayerMask enemyMask; 
+    [SerializeField] private float spawnDistance = 6;
+    [SerializeField] private float spawnHeight = 2;
+
+    [Header("Damage Settings")]
+    public float fartDamage = 10f;        
+
+    [Header("Feedback")]
+    public ParticleSystem extraVFX;       
+
     public override void Execute(GameObject player, Player playerClass)
     {
-        if (!isReady) return;//Verify if it's ready to use the skill
+        if (!isReady) return;
         StartCooldown();
-        if (!fartZone) 
+
+        //Calcula punto de spawn
+        Vector3 spawnPoint = player.transform.position + player.transform.forward * spawnDistance + Vector3.up * spawnHeight;
+
+        //Instancia el prefab principal
+        if (fartZone != null)
         {
-            
-            Vector3 spawnPoint = player.transform.position + player.transform.position * 2;
-            GameObject spawnArea = Object.Instantiate(fartZone, spawnPoint, Quaternion.identity);
-            AreaDamage areaCode = spawnArea.GetComponent<AreaDamage>();
-            if (areaCode != null)
+            GameObject zone = Instantiate(fartZone, spawnPoint, Quaternion.identity);
+            Destroy(zone, fartDuration);
+        }
+        else
+        {
+            Debug.LogError("no has asignado el prefab fartZone");
+        }
+        
+        if (extraVFX != null)//Inicializa las particulas
+            Instantiate(extraVFX, spawnPoint, Quaternion.identity).Play();
+        
+        Collider[] hits = Physics.OverlapSphere(spawnPoint, fartRadius, enemyMask);
+        int count = 0;
+        foreach (var col in hits)
+        {
+            if (col.TryGetComponent<Dummy1>(out var dummy))
             {
-                areaCode.Initialize(fartDamage, fartDuration);
+                dummy.TakeDamage(fartDamage);
+                count++;
             }
-            
-            //currentCooldown = coolDown;
         }
     }
 }
